acd88f44b4a3577a5c5142822bbb684a1d751d91
famasoon <famasoon@gmail.com>
2019-03-07 20:52:33 +0900

    blog info

---
+++ testblog/plugins/contentful.js
@@ -0,0 +1,13 @@
+const contentful = require('contentful')
+
+const config = {
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_TOKEN
+}
+
+
+module.exports = {
+    createClient () {
+        return contentful.createClient(config)
+    }
+}
